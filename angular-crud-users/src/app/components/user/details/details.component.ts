ee962269d91487f5e2130b111e89f35402b6351a
Gerson Carvalho <glcarvalho@magnasistemas.com.br>
2020-04-21 17:21:55 -0300

    projeto angular

---
+++ angular-crud-users/src/app/components/user/details/details.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../../../models/user/user';
+import { UserService } from '../../../services/user/user.service';
+//import { EmployeeListComponent } from '../employee-list/employee-list.component';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent implements OnInit {
+
+  id: number;
+  user: User;
+
+  constructor(private route: ActivatedRoute, private router: Router,
+    private UserService: UserService) { }
+
+  ngOnInit() {
+    this.user = new User();
+
+    this.id = this.route.snapshot.params['id'];
+
+    this.UserService.getUser(this.id)
+      .subscribe(data => {
+        console.log(data)
+        this.user = data;
+      }, error => console.log(error));
+  }
+
+  list() {
+    this.router.navigate(['users']);
+  }
+}
